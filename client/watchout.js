8b8ed4b4e5e0926dff1a6ecf7f65bb905201a8c3
shivg <docshivaguru@gmail.com>
2016-01-22 16:54:30 -0800

    pre tapout commit

--- client/watchout.js
+++ client/watchout.js
@@ -1,12 +1,31 @@
+
+//global variables
+var currentScore = 0;
+var highScore = 0;
+var collisions = 0;
+
 // start slingin' some d3 here.
 var Player = function() {
   this.x = 0.5;
   this.y = 0.5;
+  this.r = 0.025;
+};
+
+
+Player.prototype.collisionCheck = function(asteroid) {
+  var asteroidX = asteroid.x + asteroid.side / 2;
+  var asteroidY = asteroid.y + asteroid.side / 2;
+  var asteroidR = asteroid.side / 2;
+  var distance = Math.pow(this.x - asteroidX, 2) + Math.pow(this.y - asteroidY, 2);
+  var check1 = Math.pow(this.r - asteroidR, 2) <= distance;
+  var check2 = Math.pow(this.r + asteroidR, 2) >= distance;
+  return check1 && check2;
 };
 
 var Asteroid = function() {
   this.x = Math.random() * 0.8 + 0.1;
   this.y = Math.random() * 0.8 + 0.1;
+  this.side = 0.1;
 };
 
 Asteroid.prototype.move = function(){
@@ -40,8 +59,12 @@ svg.selectAll('.asteroid')
   .attr("y", function (asteroid) {
     return asteroid.y * height+"px";
   })
-  .attr("width", "50px")
-  .attr("height", "50px")
+  .attr("width", function(d) {
+    return d.side * width + "px";
+  })
+  .attr("height", function(d) {
+    return d.side * height + "px";
+  })
   .attr('class', 'asteroid');
 
 //make asteroids move
@@ -59,18 +82,41 @@ var asteroidMove = function () {
         return asteroid.y * height+"px";
       });
   });
+  return false;
 };
 
 setInterval(asteroidMove, 1000);
+setInterval(function () {
 
+  for (var i = 0; i < asteroids.length; i++) {
+    if(player.collisionCheck(asteroids[i])){
+      //set high score to current score
+      highScore = currentScore > highScore ? currentScore : highScore;
+      //set current score to 0
+      currentScore = 0;
+      //collisions++
+      d3.select('.highscore span').text(highScore);
+      d3.select('.current span').text(currentScore);
+      break;
+
+    } else {
+      currentScore++;
+      d3.select('.current span').text(currentScore);
+    }
+  }
+}, 10);
 //create player
 var player = new Player();
 
 var drag = d3.behavior.drag()
               .on('drag', function() {
+                var cx = d3.event.x;
+                var cy = d3.event.y;
+                player.x = cx / width;
+                player.y = cy / height;
                 d3.select(this)
-                .attr('cx', d3.event.x)
-                .attr('cy', d3.event.y);
+                .attr('cx', cx)
+                .attr('cy', cy);
               });
 //add player to board
 svg.selectAll('.player')
@@ -83,7 +129,9 @@ svg.selectAll('.player')
   .attr("cy", function (player) {
     return player.y * height+"px";
   })
-  .attr("r", "10px")
+  .attr("r", function(player) {
+    return player.r * (height + width) / 2 + 'px';
+  })
   .call(drag)
   .classed('.player');
 
